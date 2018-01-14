819bbb6ce9e2b39018f6f576736c6bac71395b48
Muqriz <muqriz19othman@gmail.com>
2018-01-14 13:41:42 +0800

    New Branch

--- scripts/script.js
+++ scripts/script.js
@@ -1,34 +1,41 @@
 var gameRunning = false;
+var randomNumber;
+var playerLives = 5;
 
-        var randomNumber;
+function startGame() {
 
-        function startGame() {
+    /*
+        Set the game variables
 
-            document.getElementById('messages').innerHTML = "What is the random number?";
-            gameRunning = true;
-            randomNumber = Math.trunc(Math.random() * 10 + 1);
+    */
+    document.getElementById('messages').innerHTML = "What is the random number?";
+    document.getElementById('playerLives').innerHTML = playerLives;
+    document.getElementById('playerLivesMessage').style.display = "block";
+    gameRunning = true;
+    randomNumber = Math.trunc(Math.random() * 10 + 1);
 
-            console.log(randomNumber);
 
+    //console.log(randomNumber);
+
+}
+
+function guessNumber() {
+
+    if (randomNumber === undefined) {
+        document.getElementById('messages').innerHTML = "Random number has not been generated... Click the \"Generate Random Number\" button to do so.";
+
+    } else {
+
+        var guessElement = document.getElementById('guesses');
+
+        var guess = Number(guessElement.value);
+        console.log(guess);
+
+        if (guess === randomNumber) {
+            document.getElementById('messages').innerHTML = "Correct! You guessed the number correct!";
+
+        } else {
+            document.getElementById('messages').innerHTML = "Keep guessing...";
         }
-
-        function guessNumber() {
-
-            if (randomNumber === undefined) {
-                document.getElementById('messages').innerHTML = "Random number has not been generated... Click the \"Generate Random Number\" button to do so.";
-
-            } else {
-                
-                var guessElement = document.getElementById('guesses');
-
-                var guess = Number(guessElement.value);
-                console.log(guess);
-
-                if (guess === randomNumber) {
-                    document.getElementById('messages').innerHTML = "Correct! You guessed the number correct!";
-
-                } else {
-                    document.getElementById('messages').innerHTML = "Keep guessing...";
-                }
-            }
-        }
+    }
+}
